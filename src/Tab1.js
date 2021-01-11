e7616353592b44fac275a820b0b33c00964327ad
gaurav.dhenge <gauravmd@celstream.com>
2021-01-11 12:05:13 +0530

    added features

--- src/Tab1.js
+++ src/Tab1.js
@@ -49,23 +49,25 @@ export default class Tab1 extends Component {
   render() {
     const sample = this.state.details;
     const showData = this.state.showmoreData;
+
     let sample2 = [];
-    let showData1 =[];
     if(sample.data){
        sample2 = sample.data.Search;
     }
-   
-    if(showData){
-      showData1 = showData.data;
-    }
-    console.log("showmoreData",this.state.showmoreData);
-    console.log("showmoreData1",showData1);
-    return(<div className="addmargin">
+     return(<div className="addmargin">
     <form>
       <Modal show={this.state.show} handleClose={this.hideModal}>
-     <p>data added</p>
+          <div> <h4>Actor:</h4> {showData.data && showData.data ? showData.data.Actors :''}</div>
+          <div> <h4>Awards:</h4> {showData.data && showData.data ? showData.data.Awards :''}</div>
+          <div> <h4>BoxOffice:</h4> {showData.data && showData.data ? showData.data.BoxOffice :''}</div>
+          <div> <h4>Director:</h4> {showData.data && showData.data ? showData.data.Director :''}</div>
+          <div> <h4>Plot:</h4> {showData.data && showData.data ? showData.data.Plot :''}</div>
+          <div> <h4>Ratings:</h4> {showData.data && showData.data ? showData.data.imdbRating :''}</div>
+         
+          { (showData.data && showData.data && showData.data.imdbRating > 7)
+                        ? <div> <h4>boxoffice: hit</h4> </div> 
+                        : <div> <h4>boxoffice: flop</h4> </div> }
       </Modal>
-
       <p> <h4>Please Enter Movie Title and Movie Release Year:</h4></p>
       <div>
       <input
@@ -73,34 +75,33 @@ export default class Tab1 extends Component {
         placeholder='Movie Title'
         onChange={this.myChangeHandler}/>
          </div>
- 
       <div>
        <input
         type='number'
         placeholder='Movie Release Year'
         onChange={this.myChangeHandler1}/>
        </div>
-
         <div>
         <button type='button' onClick={() => this.getData(this.state.username,this.state.year)} disabled={!this.state.username && !this.state.year}>save</button>
         </div>
-        
-        {sample2.map((item, idx) => {
-                 return (
-                  <div class="addMargin">
-                  <div class="card">
-                    {
-                      (item.Poster === 'N/A')
-                        ? <div> No Poster Available </div> 
-                        : <div> <img src={item.Poster} alt="Avatar"/> </div> }
-                   
-                   <div class="container">
-                   <h4><b>{item.Title}</b></h4> 
-                   <p>{item.Year}</p> 
-                   <button type='button' onClick = {()=>this.clickShow(item.imdbID)}>show more </button>
-                   </div>
-                  </div>
-                  </div>)})}
+        {(sample2 && sample2 !== 'undefined')
+        ? (sample2.map((item, idx) => {
+          return (
+           <div class="addMargin">
+           <div class="card">
+             {
+               (item.Poster === 'N/A')
+                 ? <div> No Poster Available </div> 
+                 : <div> <img src={item.Poster} alt="Avatar"/> </div> }
+            
+            <div class="container">
+            <h4><b>{item.Title}</b></h4> 
+            <p>{item.Year}</p> 
+            <button type='button' onClick = {()=>this.clickShow(item.imdbID)}>show more </button>
+            </div>
+           </div>
+           </div>)}))
+        : <p> No Data Available </p>}
       </form>
      </div>)
   }
